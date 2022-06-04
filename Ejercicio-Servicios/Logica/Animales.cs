938488b78d468cc35c6181c8d8bb343fbf6061f1
DAR-LAB-MA\UCSE <a>
2022-06-03 21:02:23 -0300

    Comienzo del ej

---
+++ Ejercicio-Servicios/Logica/Animales.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class Animales
+    {
+        //ID; Nombre, Especie, Fecha Creación, Fecha modificación, Eliminado = true / false.
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public bool Eliminado { get; set; }
+
+        public Animales(int iD, string nombre, string especie, DateTime fechaModificacion)
+        {
+            ID = iD; //hacer autonumerico
+            Nombre = nombre; //Obligatorio
+            Especie = especie; //Obligatorio
+            FechaCreacion = DateTime.Now; 
+            FechaModificacion = fechaModificacion;
+            Eliminado = false;
+        }
+    }
+}
---
+++ Ejercicio-Servicios/WebApplication1/Controllers/AnimalesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class Animales
+    {
+        //ID; Nombre, Especie, Fecha Creación, Fecha modificación, Eliminado = true / false.
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public bool Eliminado { get; set; }
+
+        public Animales(int iD, string nombre, string especie, DateTime fechaModificacion)
+        {
+            ID = iD; //hacer autonumerico
+            Nombre = nombre; //Obligatorio
+            Especie = especie; //Obligatorio
+            FechaCreacion = DateTime.Now;
+            FechaModificacion = fechaModificacion;
+            Eliminado = false;
+        }
+    }
+
+    public class AnimalesController : ApiController
+    {
+        private static List<Animales> ListaAnimales = new List<Animales>();
+
+        public IHttpActionResult Post(string nombre, string especie, DateTime fechaModificacion)
+        {
+            Animales animal = new Animales(ListaAnimales.Count+1, nombre, especie, fechaModificacion);
+
+            return Created(nombre, animal);
+        }
+
+    }
+}
